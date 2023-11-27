20e386461c37c2810c1c896c0786192a756bbed8
Louka <louka.cipolat@yahoo.fr>
2023-11-27 18:54:28 +0000

    colo submit-btn

--- SHRS/src/app/components/submit-btn.tsx
+++ SHRS/src/app/components/submit-btn.tsx
@@ -10,7 +10,7 @@ export default function SubmitBtn() {
   return (
     <button 
             type='submit' 
-            className='group flex flex-row justify-center items-center gap-2 mt-4 bg-primary-color w-[8rem] h-[3rem] rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover-secondary-color active:scale-105 disabled:scale-100 disabled:bg-opacity-65 hover:text-white'
+            className='group flex flex-row justify-center items-center gap-2 mt-4 text-white bg-secondary-color w-[8rem] h-[3rem] rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 disabled:scale-100 disabled:bg-opacity-65 hover-secondary-color-text hover-primary-color'
             disabled={pending}
         >{
             pending ? <div className='h-5 w-5 animate-spin rounded-full border-b-2 border-white'></div>:(
